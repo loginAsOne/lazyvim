43d67c9257f2d06fb8dc7fb70f2cfaf96f220ea7
loginAsOne <loginasone@gmail.com>
2025-05-31 22:05:38 -0400

    added back java

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -32,6 +32,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.scala" },
     { import = "lazyvim.plugins.extras.lang.ansible" },
+    { import = "lazyvim.plugins.extras.lang.java" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     -- { import = "plugins.lang.scala" },
